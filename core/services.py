a917f327cf376d0c439dc363d749ee5aa8ab4664
amir <a.amiri@hami.blue>
2021-01-11 16:14:18 +0330

    refactor services

--- core/services.py
+++ core/services.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 import logging
-import os
-import re
-import subprocess
+import sys
+
 import requests
 
 from pathlib import Path
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, text, Boolean
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, text
 
-from core.consts import AWS_PROXY_API, AWS_PROXY_API_TOKEN, NETCAT_TIMEOUT
+from core.consts import AWS_PROXY_API, AWS_PROXY_API_TOKEN
 
 logger = logging.getLogger(__name__)
 
@@ -24,26 +22,21 @@ class DBService(object):
         self.meta = MetaData()
         self.table_name = 'Servers'
         self.conn = self.engine.connect()
-        self.session = sessionmaker(bind=self.engine)
-
-    def __table(self):
-        table = Table(
+        self.__table = Table(
             self.table_name, self.meta,
             Column('id', Integer, primary_key=True),
             Column('server_id', Integer),
             Column('hash_key', String),
         )
-
         self.meta.create_all(self.engine)
-        return table
 
     def insert(self, obj_id, obj_hash_key):
-        insert = self.__table().insert().values(server_id=obj_id, hash_key=obj_hash_key)
+        insert = self.__table.insert().values(server_id=obj_id, hash_key=obj_hash_key)
         self.conn.execute(insert)
 
     def exists(self, server_id, hash_key):
         raw = text(
-            f"SELECT count('id') FROM {self.table_name} where hash_key={hash_key} and server_id={server_id}"
+            f"SELECT count('id') FROM {self.table_name} where hash_key='{hash_key}' and server_id='{server_id}'"
         )
         result = self.conn.execute(raw).fetchone()[0]
         return result > 0
@@ -57,74 +50,28 @@ class APIService(object):
     SERVER_INSPECT_URL = f'{AWS_PROXY_API}/api/v1/inspectors/inspected-server/'
     SERVER_LIST_URL = f'{AWS_PROXY_API}/api/v1/inspectors/inquiry-server/'
 
-    def api_call(self, method, url, data=None, params=None, files=None, **kwargs):
+    def custom_request(self, url, method='post', **kwargs):
         try:
-            if method.lower() == 'get':
-                response = requests.get(url=url, headers=self.HEADERS, params=params, **kwargs)
-            elif method.lower() == 'post':
-                response = requests.post(url=url, headers=self.HEADERS, json=data, files=files, **kwargs)
-            elif method.lower() == 'patch':
-                response = requests.patch(url=url, headers=self.HEADERS, json=data, files=files, **kwargs)
-            else:
-                return None
+            logger.debug(f"[making request]-[method: {method}]-[URL: {url}]-[kwargs: {kwargs}]")
+            response = requests.request(method, url, headers=self.HEADERS, **kwargs)
             response.raise_for_status()
-            return response
+            result = response.json()
         except requests.exceptions.HTTPError as e:
-            logger.error(f'http error for requesting url {url} occurred: {e}')
-            if e.response.status_code == 400:
-                raise Exception(e.response.text)
-            raise e
-        except requests.exceptions.RequestException as e:
-            logger.error(f'request error for requesting url {url} occurred: {e}')
-            raise e
+            sys.stderr.write(
+                f'[making request failed]-[response err: {e.response.text}]-[status code: {e.response.status_code}]'
+                f'-[URL: {url}]-[exc: {e}]'
+            )
+            raise Exception(e.response.text)
+        except requests.exceptions.ConnectTimeout as e:
+            sys.stderr.write(f'[request failed]-[URL: {url}]-[exc: {e}]')
+            raise
+        except Exception as e:
+            sys.stderr.write(f'[request failed]-[URL: {url}]-[exc: {e}]')
+            raise
+        return result
 
     def get_servers_list(self):
-        return self.api_call('get', self.SERVER_LIST_URL).json()
+        return self.custom_request(self.SERVER_LIST_URL, method='get')
 
     def post_server_status(self, data):
-        return self.api_call(method='post', url=self.SERVER_INSPECT_URL, data=data)
-
-
-class PingCheck:
-
-    def __init__(self, input_string):
-        self.input_string = input_string
-        self.is_ping = False
-        self.ip = None
-        self.time = None
-        self.success = 0
-        self.domain = None
-        self.ping()
-
-    def ping(self):
-        ip_pattern = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-        is_ip = ip_pattern.match(self.input_string)
-
-        if is_ip is None:
-            self.domain = self.input_string
-        else:
-            self.ip = self.input_string
-            self.domain = ''
-
-        try:
-            ping = os.popen(f'ping -c 6 -q -s 1 {self.input_string}').read()
-            self.ip = ping.split('\n')[0].split()[2][1:-1]
-            self.time = ping.split('\n')[-3].split()[-1].lstrip('time')
-            self.success = 100.0 - float(ping.split('\n')[-3].split(',')[2].rstrip('% packet loss'))
-        except Exception as e:
-            print(f"{self.domain or self.ip} {e}")
-        return self.success >= 60.0
-
-
-class NetcatCheck:
-
-    def __init__(self, ip, port):
-        self.is_ping = False
-        self.nc(ip, port)
-
-    def nc(self, ip, port):
-        cmd = f"netcat -v -z -w{NETCAT_TIMEOUT} {ip} {port}"
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        res = str(process.stderr.read())
-
-        self.is_ping = 'succeeded' in res
+        return self.custom_request(url=self.SERVER_INSPECT_URL, method='post', json=data)
--- main.py
+++ main.py
@@ -1,28 +1,58 @@
 #!/usr/bin/env python3
+import os
+import re
+import sys
+import subprocess
 
-import logging
+from core.services import DBService, APIService
+from core.consts import SERVER_PORT, NETCAT_TIMEOUT
 
-from core.services import DBService, APIService, NetcatCheck, PingCheck
-from core.consts import SERVER_PORT
 
-logger = logging.getLogger(__name__)
+def pinged(input_string):
+    ip_pattern = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+    is_ip = ip_pattern.match(input_string)
+    success = 0
+
+    if is_ip is None:
+        domain = input_string
+        ip = None
+    else:
+        ip = input_string
+        domain = ''
+
+    try:
+        ping = os.popen(f'ping -c 6 -q -s 1 {input_string}').read()
+        ip = ping.split('\n')[0].split()[2][1:-1]
+        success = 100.0 - float(ping.split('\n')[-3].split(',')[2].rstrip('% packet loss'))
+    except Exception as e:
+        print(f"{domain or ip} {e}")
+    return success >= 60.0
+
+
+def nc(ip, port):
+    cmd = f"netcat -v -z -w{NETCAT_TIMEOUT} {ip} {port}"
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = str(process.stderr.read())
+    return 'succeeded' in res
 
 
 def check_server_connection():
+    db = DBService('inspector')
     response = APIService().get_servers_list()
     for server in response:
         try:
-            ping_status = NetcatCheck(server['ip'], SERVER_PORT).is_ping or PingCheck(server['ip']).is_ping
+            if db.exists(server['id'], server['hash_key']):
+                continue
 
+            ping_status = nc(server['ip'], SERVER_PORT) or pinged(server['ip'])
             APIService().post_server_status(data={
                 "hash_key": server['hash_key'],
                 "server": server['id'],
                 "is_active": ping_status
             })
-            DBService('inspector').insert(server['id'], server['hash_key'])
+            db.insert(server['id'], server['hash_key'])
         except Exception as e:
-            logger.error(f'error in checking server connection {server["ip"]} : {e}')
-            continue
+            sys.stderr.write(f'error in checking server connection {server["ip"]} : {e}')
 
 
 if __name__ == "__main__":
